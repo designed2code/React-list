101b0e11d639e1d5f70f102c48f868c088ffb713
designed2code <destined2code@gmail.com>
2021-09-15 20:29:42 +0530

    Create LineItem Component, Pass data from ItemList to LineItem

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,23 +1,14 @@
-import {FaTrashAlt} from "react-icons/fa"
+import LineItem from "./LineItem";
+
 const ItemList = ({items, handleCheck, deleteHandler}) => {
     return ( 
         <ul>
                 {items.map((item)=> (
-                    <li className="item" key={item.id}>
-                        <input type="checkbox"
-                        onChange = {() => handleCheck(item.id)}
-                        checked={item.checked}
-                        />
-                        <label
-                        style = {(item.checked) ? {textDecoration: 'line-through'}: null}
-                        onDoubleClick = {() => handleCheck(item.id)}
-                        >{item.item}</label>
-                        <FaTrashAlt 
-                            role="button" 
-                            tabIndex = "0"
-                            onClick= {()=>deleteHandler(item.id)}
-                        />
-                    </li>
+                    <LineItem
+                    item = {item}
+                    handleCheck = {handleCheck}
+                    deleteHandler = {deleteHandler}
+                    />
                 ))}
             </ul>
      );
---
+++ src/LineItem.js
@@ -0,0 +1,22 @@
+import {FaTrashAlt} from "react-icons/fa"
+const LineItem = ({item, handleCheck, deleteHandler}) => {
+    return ( 
+        <li className="item" key={item.id}>
+                        <input type="checkbox"
+                        onChange = {() => handleCheck(item.id)}
+                        checked={item.checked}
+                        />
+                        <label
+                        style = {(item.checked) ? {textDecoration: 'line-through'}: null}
+                        onDoubleClick = {() => handleCheck(item.id)}
+                        >{item.item}</label>
+                        <FaTrashAlt 
+                            role="button" 
+                            tabIndex = "0"
+                            onClick= {()=>deleteHandler(item.id)}
+                        />
+                    </li>
+     );
+}
+ 
+export default LineItem;
