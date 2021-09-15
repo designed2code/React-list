e41e62a6b7905afc42071a1fc324370fa4bba81e
designed2code <destined2code@gmail.com>
2021-09-15 19:54:19 +0530

    Data and function passing from App to Content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import {useState} from "react"
+
 function App() {
+
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: true,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ])
+
+    const handleCheck = (id) => {
+      // CheckBox Logic
+      // If the item is already checked and if we click again then make it false with !item.checked else check the item
+      const listItems = items.map((item) => item.id === id ? {...item,checked: !item.checked} : item)
+      console.log(listItems)
+      setItems(listItems)
+      localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
+  const deleteHandler = (id) => {
+      // Delete Logic 
+      // So for example if you click on first element that is your id = 1 and item.id = 1
+      const listItems = items.filter((item) => item.id !== id)
+      setItems(listItems)
+      localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
+
   return (
     <div className='App'>
     <Header/>
-    <Content/>
+    <Content
+    items = {items}
+    handleCheck = {handleCheck}
+    deleteHandler = {deleteHandler}/>
     <Footer/>
     </div>
     
